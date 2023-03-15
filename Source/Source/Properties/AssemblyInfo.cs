98d067003ba75b0855812a9f403b61af6415c3ea
wyq <1163429473@qq.com>
2023-03-16 01:52:21 +0800

    update assembly version

---
+++ Source/Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.4.1")]
+[assembly: AssemblyTitle("One-click convert after battle")]
+[assembly: AssemblyDescription("一键转化战利品")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alfie Brook")]
+[assembly: AssemblyProduct("One-click convert after battle")]
+[assembly: AssemblyCopyright("Copyright Alfie Brook,Baidu Tieba&Ludeon forum")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("A3C5EF92-C552-4B96-A0D7-2DE45D2AFD93")]
+[assembly: AssemblyFileVersion("1.4.1")]
